7f94fe16d8a22cc208faf11ca0d75d9d21fdbf01
dev <dev@brigid.jp>
2019-10-25 17:25:30 +0900

    OpenSSL実装を修正。

--- evp.cpp
+++ evp.cpp
@@ -5,6 +5,20 @@
 #include <openssl/err.h>
 
 namespace brigid {
+  int impl_encrypt(lua_State* L) {
+    return 0;
+  }
+
   void initialize_evp(lua_State* L) {
+    lua_newtable(L);
+    lua_pushstring(L, "evp");
+    lua_pushvalue(L, -2);
+    lua_settable(L, -4);
+
+    lua_pushstring(L, "encrypt");
+    lua_pushcfunction(L, impl_encrypt);
+    lua_settable(L, -3);
+
+    lua_pop(L, 1);
   }
 }
--- test/test.lua
+++ test/test.lua
@@ -1,3 +1,8 @@
 local brigid_core = require "brigid_core"
 
-print(brigid_core)
+if not brigid_core.evp then
+  os.exit()
+end
+
+print(brigid_core.evp)
+print(brigid_core.evp.encrypt)
