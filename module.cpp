1bbc10431c7e85426d33d4e06980e41ab8d26e73
dev <dev@brigid.jp>
2019-11-06 11:17:01 +0900

    ビルドスクリプトを修正。

--- module.cpp
+++ module.cpp
@@ -1,10 +1,18 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include "common.hpp"
 
 namespace brigid {
+#ifdef HAVE_OPENSSL
   void initialize_evp(lua_State*);
+#endif
 
   void initialize(lua_State* L) {
+#ifdef HAVE_OPENSSL
     initialize_evp(L);
+#endif
   }
 }
 
