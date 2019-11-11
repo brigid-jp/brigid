2a50b5af78642021a9c05e619c4702f315685e50
dev <dev@brigid.jp>
2019-11-11 11:19:36 +0900

    Windows実装を修正。

--- crypto_windows.cpp
+++ crypto_windows.cpp
@@ -1,8 +1,18 @@
 #include "common.hpp"
 
+#include <windows.h>
+#include <wincrypt.h>
+
+#pragma comment(lib, "advapi32.lib")
+// #pragma comment(lib, "crypt32.lib")
+
 namespace brigid {
   namespace {
     int impl_encrypt_string(lua_State* L) {
+      HCRYPTPROV prov = 0;
+      if (!CryptAcquireContext(&prov, nullptr, MS_ENH_RSA_AES_PROV, PROV_RSA_AES, CRYPT_NEWKEYSET)) {
+      }
+
       return 0;
     }
   }
