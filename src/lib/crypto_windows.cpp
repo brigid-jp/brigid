51e02d189bbcf2ea4b828f908220bf807514ae0c
dev <dev@brigid.jp>
2019-11-15 11:38:45 +0900

    ライブラリ実装を追加。

---
+++ src/lib/crypto_windows.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2019 <dev@brigid.jp>
+// This software is released under the MIT License.
+// https://opensource.org/licenses/mit-license.php
+
+#include <brigid/crypto.hpp>
+
+#include <stdexcept>
+
+namespace brigid {
+
+  std::unique_ptr<encryptor_impl> make_encryptor_impl(const std::string& cipher, const char* key_data, size_t key_size, const char* iv_data, size_t iv_size) {
+    throw std::runtime_error("unsupported cipher");
+  }
+}
--- windows.mk
+++ windows.mk
@@ -3,6 +3,8 @@
 # https://opensource.org/licenses/mit-license.php
 
 OBJS = \
+	src/lib/crypto.obj \
+	src/lib/crypto_windows.obj \
 	common.obj \
 	module.obj \
 	crypto_windows.obj
@@ -10,14 +12,11 @@ TARGET = brigid.dll
 
 all: $(TARGET)
 
-clean:
-	del $(OBJS) $(TARGET)
-
 $(TARGET): $(OBJS)
 	link $(LIBFLAG) /DEF:brigid.def /OUT:$@ $(LUA_LIBDIR)\$(LUALIB) $**
 
 .cpp.obj:
-	$(CC) $(CFLAGS) /I$(LUA_INCDIR) /c $<
+	$(CC) $(CFLAGS) /I$(LUA_INCDIR) /Iinclude /c $< /Fo$@
 
 install:
 	xcopy "$(TARGET)" "$(LIBDIR)"
