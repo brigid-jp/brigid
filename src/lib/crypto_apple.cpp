8b88aa50ac13ae8acdae1753222ee2802b9f80df
dev <dev@brigid.jp>
2019-11-20 09:24:47 +0900

    リファクタリング。

--- src/lib/crypto_apple.cpp
+++ src/lib/crypto_apple.cpp
@@ -49,29 +49,6 @@ namespace brigid {
     private:
       cryptor_ref_t cryptor_;
     };
-
-    class aes_decryptor_impl : public cryptor {
-    public:
-      aes_decryptor_impl(const char* key_data, size_t key_size, const char* iv_data, size_t)
-        : cryptor_(make_cryptor_ref()) {
-        CCCryptorRef cryptor = nullptr;
-        check(CCCryptorCreateWithMode(kCCDecrypt, kCCModeCBC, kCCAlgorithmAES, kCCOptionPKCS7Padding, iv_data, key_data, key_size, nullptr, 0, 0, 0, &cryptor));
-        cryptor_ = make_cryptor_ref(cryptor);
-      }
-
-      virtual size_t update(const char* in_data, size_t in_size, char* out_data, size_t out_size, bool padding) {
-        size_t size1 = 0;
-        size_t size2 = 0;
-        check(CCCryptorUpdate(cryptor_.get(), in_data, in_size, out_data, out_size, &size1));
-        if (padding) {
-          check(CCCryptorFinal(cryptor_.get(), out_data + size1, out_size - size1, &size2));
-        }
-        return size1 + size2;
-      }
-
-    private:
-      cryptor_ref_t cryptor_;
-    };
   }
 
   std::unique_ptr<cryptor> make_encryptor(const std::string& cipher, const char* key_data, size_t key_size, const char* iv_data, size_t iv_size) {
--- src/lib/crypto_impl.cpp
+++ src/lib/crypto_impl.cpp
@@ -8,29 +8,24 @@
 
 namespace brigid {
   void check_cipher(const std::string& cipher, size_t key_size, size_t iv_size) {
+    size_t aes_key_size = 0;
+    size_t aes_block_size = 16;
+
     if (cipher == "aes-128-cbc") {
-      if (key_size != 16) {
-        throw std::runtime_error("invalid key size");
-      }
-      if (iv_size != 16) {
-        throw std::runtime_error("invalid iv size");
-      }
+      aes_key_size = 16;
     } else if (cipher == "aes-192-cbc") {
-      if (key_size != 24) {
-        throw std::runtime_error("invalid key size");
-      }
-      if (iv_size != 16) {
-        throw std::runtime_error("invalid iv size");
-      }
+      aes_key_size = 24;
     } else if (cipher == "aes-256-cbc") {
-      if (key_size != 32) {
-        throw std::runtime_error("invalid key size");
-      }
-      if (iv_size != 16) {
-        throw std::runtime_error("invalid iv size");
-      }
+      aes_key_size = 32;
     } else {
       throw std::runtime_error("unsupported cipher");
+    }
+
+    if (key_size != aes_key_size) {
+      throw std::runtime_error("invalid key size");
+    }
+    if (iv_size != aes_block_size) {
+      throw std::runtime_error("invalid iv size");
     }
   }
 }
