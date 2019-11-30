85439e6eb5ac4566c38558ba64099488813d0fa5
dev <dev@brigid.jp>
2019-11-30 10:42:57 +0900

    リファクタリング。

--- src/lib/java.cpp
+++ src/lib/java.cpp
@@ -5,7 +5,8 @@
 #include "error.hpp"
 #include "java.hpp"
 
-#include <string>
+#include <jni.h>
+
 #include <vector>
 
 extern "C" void* SDL_AndroidGetJNIEnv();
@@ -24,14 +25,15 @@ namespace brigid {
       get_env()->DeleteGlobalRef(object);
     }
 
-    void check() {
-      static const char* what = "crypto_java exception";
-
+    void check_impl(bool null_pointer) {
       JNIEnv* env = get_env();
       if (!env->ExceptionCheck()) {
+        if (null_pointer) {
+          throw BRIGID_ERROR("null pointer");
+        }
         return;
       }
-      local_ref_t<jthrowable> exception = make_local_ref(env->ExceptionOccurred());
+      local_ref_t<jthrowable> instance = make_local_ref(env->ExceptionOccurred());
       env->ExceptionClear();
 
       local_ref_t<jclass> klass = make_local_ref(env->FindClass("java/lang/Throwable"));
@@ -40,7 +42,7 @@ namespace brigid {
         klass.reset();
       }
       if (!klass) {
-        throw std::runtime_error(what);
+        throw BRIGID_ERROR("cannot FindClass");
       }
 
       jmethodID method = env->GetMethodID(klass.get(), "toString", "()Ljava/lang/String;");
@@ -49,30 +51,34 @@ namespace brigid {
         method = nullptr;
       }
       if (!method) {
-        throw std::runtime_error(what);
+        throw BRIGID_ERROR("cannot GetMethodID");
       }
 
-      local_ref_t<jstring> result = make_local_ref(reinterpret_cast<jstring>(env->CallObjectMethod(exception.get(), method)));
+      local_ref_t<jstring> result = make_local_ref(reinterpret_cast<jstring>(env->CallObjectMethod(instance.get(), method)));
       if (env->ExceptionCheck()) {
         env->ExceptionClear();
         result.reset();
       }
       if (!result) {
-        throw std::runtime_error(what);
+        throw BRIGID_ERROR("cannot CallObjectMethod");
       }
 
-      std::vector<char> buffer(env->GetStringUTFLength(result.get()));
+      std::vector<char> buffer(env->GetStringUTFLength(result.get()) + 1);
       env->GetStringUTFRegion(result.get(), 0, env->GetStringLength(result.get()), buffer.data());
       if (env->ExceptionCheck()) {
         env->ExceptionClear();
-        throw std::runtime_error(what);
+        throw BRIGID_ERROR("cannot GetStringUTFRegion");
       }
 
-      throw std::runtime_error("crypto_java exception " + std::string(buffer.begin(), buffer.end()));
+      throw BRIGID_ERROR(buffer.data());
     }
 
-    jboolean to_boolean(bool value) {
-      if (value) {
+    void check() {
+      check_impl(false);
+    }
+
+    jboolean to_boolean(bool source) {
+      if (source) {
         return JNI_TRUE;
       } else {
         return JNI_FALSE;
--- src/lib/java.hpp
+++ src/lib/java.hpp
@@ -22,7 +22,7 @@ namespace brigid {
     using local_ref_t = std::unique_ptr<remove_pointer_t<T>, decltype(&delete_local_ref)>;
 
     template <class T>
-    inline local_ref_t<T> make_local_ref(T object = nullptr) {
+    inline local_ref_t<T> make_local_ref(T object, enable_if_t<std::is_base_of<remove_pointer_t<jobject>, remove_pointer_t<T>>::value>* = nullptr) {
       return local_ref_t<T>(object, &delete_local_ref);
     }
 
@@ -32,18 +32,22 @@ namespace brigid {
     using global_ref_t = std::unique_ptr<remove_pointer_t<T>, decltype(&delete_global_ref)>;
 
     template <class T>
-    inline global_ref_t<T> make_global_ref(T object = nullptr) {
+    inline global_ref_t<T> make_global_ref(enable_if_t<std::is_base_of<remove_pointer_t<jobject>, remove_pointer_t<T>>::value>* = nullptr) {
+      return global_ref_t<T>(nullptr, &delete_global_ref);
+    }
+
+    template <class T>
+    inline global_ref_t<T> make_global_ref(T object, enable_if_t<std::is_base_of<remove_pointer_t<jobject>, remove_pointer_t<T>>::value>* = nullptr) {
       return global_ref_t<T>(object, &delete_global_ref);
     }
+
+    void check_impl(bool);
 
     void check();
 
     template <class T>
-    T check(T result) {
-      check();
-      if (!result) {
-        throw std::runtime_error("crypto_java exception");
-      }
+    T* check(T* result) {
+      check_impl(!result);
       return result;
     }
 
