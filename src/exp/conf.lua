4dd776ddd0ffbad85deb8eb1a8e6c0a94618b6b1
dev <dev@brigid.jp>
2019-12-21 16:26:00 +0900

    実験コードを削除。

--- src/exp/conf.lua
+++
@@ -1,9 +0,0 @@
--- Copyright (c) 2019 <dev@brigid.jp>
--- This software is released under the MIT License.
--- https://opensource.org/licenses/mit-license.php
-
-function love.conf(t)
-  t.gammacorrect = true
-  t.window.resizable = true
-  t.window.highdpi = true
-end
