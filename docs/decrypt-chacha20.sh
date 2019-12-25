614c8738d95dda881cb31cb7fd138e874cf64fd2
dev <dev@brigid.jp>
2019-12-25 15:23:22 +0900

    文書を追加。

---
+++ docs/decrypt-chacha20.sh
@@ -0,0 +1,19 @@
+#! /bin/sh -e
+
+# Copyright (c) 2019 <dev@brigid.jp>
+# This software is released under the MIT License.
+# https://opensource.org/licenses/mit-license.php
+
+plain="The quick brown fox jumps over the lazy dog"
+# 256bit: "01234567890123456789012345678901"
+key=3031323334353637383930313233343536373839303132333435363738393031
+# 128bit: "0123456778012345"
+iv=30313233343536373839303132333435
+
+result=`openssl enc -d -chacha -in chacha20.dat -K "$key" -iv "$iv"`
+if test "X$plain" = "X$result"
+then
+  echo "PASS chacha20"
+else
+  echo "FAIL chacha20"
+fi
