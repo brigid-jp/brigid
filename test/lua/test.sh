ab0cde550630cbc19f4285c026b24a5c015fdaba
dev <dev@brigid.jp>
2019-12-26 14:52:40 +0900

    テストを修正。

--- test/lua/test.sh
+++ test/lua/test.sh
@@ -4,4 +4,7 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/mit-license.php
 
-ls test_*.lua | lua test.lua autotools
+case X$# in
+  X0) ls test_*.lua | lua test.lua autotools;;
+  *) ls test_*.lua | "$@" test.lua autotools;;
+esac
