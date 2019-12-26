7653a3d1b9638fc6c0bcb2773269b3c9d928603b
dev <dev@brigid.jp>
2019-12-26 20:51:41 +0900

    zoneの実装を開始。

---
+++ test/lua/test_zone.lua
@@ -0,0 +1,12 @@
+-- Copyright (c) 2019 <dev@brigid.jp>
+-- This software is released under the MIT License.
+-- https://opensource.org/licenses/mit-license.php
+
+local brigid = require "brigid"
+
+brigid.zone.put(5, 0xA1)
+brigid.zone.put(6, 0xB2)
+brigid.zone.put(7, 0xC3)
+brigid.zone.put(8, 0xD4)
+
+brigid.zone.dump()
