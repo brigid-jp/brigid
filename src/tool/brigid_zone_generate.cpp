a41d2ab0be6d6ce77418fcee2a24adc3420ba7e6
dev <dev@brigid.jp>
2019-12-27 11:44:31 +0900

    zoneの生成を修正。

--- src/tool/brigid_zone_generate.cpp
+++ src/tool/brigid_zone_generate.cpp
@@ -3,34 +3,62 @@
 // https://opensource.org/licenses/mit-license.php
 
 #include <stdint.h>
-#include <stdio.h>
+#include <iomanip>
+#include <iostream>
 #include <random>
+#include <sstream>
+
+std::string encode(uint32_t value) {
+  std::ostringstream out;
+  out << "0x"
+      << std::hex
+      << std::uppercase
+      << std::setfill('0')
+      << std::setw(8)
+      << value;
+  return out.str();
+}
 
 int main(int, char*[]) {
   std::independent_bits_engine<std::random_device, 32, uint32_t> engine;
 
-  uint32_t data[] = {
-    engine(),
-    engine(),
-    engine(),
-    engine(),
-    engine(),
-    engine(),
-    engine(),
-    engine(),
+  std::string data[] = {
+    encode(engine()),
+    encode(engine()),
+    encode(engine()),
+    encode(engine()),
+    encode(engine()),
+    encode(engine()),
+    encode(engine()),
+    encode(engine()),
   };
 
+  std::ostream& out = std::cout;
+
+  out << "/*\n";
   for (int i = 0; i < 8; ++i) {
-    printf("#define BRIGID_ZONE%d 0x%08X\n", i + 1, data[i]);
+    out << "BRIGID_ZONE" << i + 1 << "=" << data[i] << "\n";
   }
+  out << "export";
+  for (int i = 0; i < 8; ++i) {
+    out << " BRIGID_ZONE" << i + 1;
+  }
+  out << "\n";
+  out << "*/\n";
 
   for (int i = 0; i < 8; ++i) {
-    printf("export BRIGID_ZONE%d=0x%08X\n", i + 1, data[i]);
+    out << "#if !(BRIGID_ZONE" << i + 1 << "+0)\n"
+        << "#define BRIGID_ZONE" << i + 1 << " " << data[i] << "\n"
+        << "#endif\n";
   }
 
-  for (int i = 0; i < 8; ++i) {
-    printf("set BRIGID_ZONE%d=0x%08X\n", i + 1, data[i]);
-  }
+  // for (int i = 0; i < 8; ++i) {
+  //   printf("export BRIGID_ZONE%d=0x%08X\n", i + 1, data[i]);
+  // }
+
+  // for (int i = 0; i < 8; ++i) {
+  //   printf("set BRIGID_ZONE%d=0x%08X\n", i + 1, data[i]);
+  // }
 
   return 0;
 }
