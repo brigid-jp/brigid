54b648d1ffd088ab97a196ad76ff1ea695d42e0a
dev <dev@brigid.jp>
2019-12-27 10:06:16 +0900

    zoneのインジェクションの実装を修正。

--- brigid_zone.sh
+++ brigid_zone.sh
@@ -4,15 +4,32 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/mit-license.php
 
-generate() {
+case X$1 in
+  Xgenerate)
+    
+
+
+
+
+    ;;
+esac
+
+# env-export
+# env-set
+
+
+
+
+
+
+# generate
+# random
+
+write() {
   case X$2 in
     X) ;;
     *) printf '#define %s 0x%08X\n' "$1" "$2";;
   esac
 }
 
-generate BRIGID_ZONE1 "$BRIGID_ZONE1"
-
-
-
-
+write BRIGID_ZONE1 "$BRIGID_ZONE1"
