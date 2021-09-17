90fda19557e72c4dab3a97d232c63e912de925b2
dev <dev@brigid.jp>
2021-09-17 09:00:46 +0900

    テストを修正。

--- test/lua/test_to_be_closed.lua
+++ test/lua/test_to_be_closed.lua
@@ -5,24 +5,22 @@
 local major, minor = assert(_VERSION:match "^Lua (%d+)%.(%d+)$")
 local version = major * 100 + minor
 
-if version < 504 then
-  os.exit()
+if version >= 504 then
+  assert(load [[
+    local brigid = require "brigid"
+
+    local writer = assert(brigid.file_writer "test.dat")
+
+    do
+      local writer_to_be_closed <close> = writer
+      assert(writer:write "foo\n")
+      assert(writer:write "bar\n")
+      assert(writer:write "baz\n")
+    end -- writer is closed
+
+    -- attempt to use a closed
+    local result, message = pcall(function () writer:write "qux\n" end)
+    print(message)
+    assert(not result)
+  ]])()
 end
-
-assert(load [[
-local brigid = require "brigid"
-
-local writer = assert(brigid.file_writer "test.dat")
-
-do
-  local writer_to_be_closed <close> = writer
-  assert(writer:write "foo\n")
-  assert(writer:write "bar\n")
-  assert(writer:write "baz\n")
-end -- writer is closed
-
--- attempt to use a closed
-local result, message = pcall(function () writer:write "qux\n" end)
-print(message)
-assert(not result)
-]])()
