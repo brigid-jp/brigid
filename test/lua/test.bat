237b47b4c7d306cb7eb472b70e3be94a49916390
dev <dev@brigid.jp>
2021-09-21 22:07:46 +0900

    テストを修正。

--- test/lua/test.bat
+++ test/lua/test.bat
@@ -4,4 +4,5 @@ REM Copyright (c) 2019,2021 <dev@brigid.jp>
 REM This software is released under the MIT License.
 REM https://opensource.org/licenses/mit-license.php
 
+SET LUA_CPATH=..\..\?.dll;;
 lua5.1 test.lua
