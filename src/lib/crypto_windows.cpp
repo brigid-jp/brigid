5c8d9f39182f7585a5b989a35ec6005fc11a30d4
dev <dev@brigid.jp>
2021-09-27 10:17:04 +0900

    SHA1のWindows実装を追加。

--- src/lib/crypto_windows.cpp
+++ src/lib/crypto_windows.cpp
@@ -323,6 +323,8 @@ namespace brigid {
 
   std::unique_ptr<hasher> make_hasher(crypto_hash hash) {
     switch (hash) {
+      case crypto_hash::sha1:
+        return std::unique_ptr<hasher>(new hasher_impl(BCRYPT_SHA1_ALGORITHM));
       case crypto_hash::sha256:
         return std::unique_ptr<hasher>(new hasher_impl(BCRYPT_SHA256_ALGORITHM));
       case crypto_hash::sha512:
