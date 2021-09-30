3a9e9af9f4e1593e995113d0d9d9a076cbb02b4c
dev <dev@brigid.jp>
2021-09-30 16:54:39 +0900

    実験用コードを追加。

---
+++ test/lab/client.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2021 <dev@brigid.jp>
+// This software is released under the MIT License.
+// https://opensource.org/licenses/mit-license.php
+
+int main(int, char*[]) {
+  return 0;
+}
