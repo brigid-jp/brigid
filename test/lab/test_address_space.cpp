6405b3ba21cbe9255ac9ba5b922706e8e9aca40b
dev <dev@brigid.jp>
2021-10-15 18:55:18 +0900

    実験用のコードを修正。

--- test/lab/test_address_space.cpp
+++ test/lab/test_address_space.cpp
@@ -7,6 +7,10 @@
 #include <string.h>
 #include <chrono>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <vector>
 
 namespace brigid {
   namespace {
@@ -88,6 +92,60 @@ namespace brigid {
       return 0;
     }
 
+    int nifty_count = 0;
+
+    struct nifty_counter {
+      nifty_counter() {
+        ++nifty_count;
+        std::cout << "nifty_count cstr " << nifty_count << " " << std::this_thread::get_id() << "\n";
+      }
+
+      ~nifty_counter() {
+        --nifty_count;
+        std::cout << "nifty_count dstr " << nifty_count << " " << std::this_thread::get_id() << "\n";
+      }
+    };
+
+    namespace {
+      nifty_counter nc;
+    }
+
+    void start_routine(const char* data, size_t size) {
+      lua_State* L = luaL_newstate();
+      luaL_openlibs(L);
+
+      // std::cout << "[" << std::string(data, size) << "]\n";
+      int result = luaL_loadbuffer(L, data, size, nullptr);
+      // std::cout << "result " << result << "\n";
+      if (result == 0) {
+        std::ostringstream out;
+        out << std::this_thread::get_id();
+        std::string id = out.str();
+        lua_pushlstring(L, id.data(), id.size());
+        int result = lua_pcall(L, 1, 0, 0);
+        if (result != 0) {
+          std::cerr << "error " << lua_tostring(L, -1) << "\n";
+        }
+      }
+      lua_close(L);
+    }
+
+    int create_native_threads(lua_State* L) {
+      lua_Integer num_threads = luaL_checkinteger(L, 1);
+      size_t size = 0;
+      const char* data = luaL_checklstring(L, 2, &size);
+
+      std::vector<std::thread> threads;
+      for (lua_Integer i = 0; i < num_threads; ++i) {
+        threads.emplace_back(std::thread(start_routine, data, size));
+      }
+      for (auto& thread : threads) {
+        thread.join();
+      }
+
+      return 0;
+    }
+
     void initialize(lua_State* L) {
       lua_pushinteger(L, sizeof(void*));
       lua_setfield(L, -2, "sizeof_void_pointer");
@@ -104,12 +162,17 @@ namespace brigid {
       lua_setfield(L, -2, "bench_get_registry02");
       lua_pushcfunction(L, bench_get_registry03);
       lua_setfield(L, -2, "bench_get_registry03");
+
+      lua_pushcfunction(L, create_native_threads);
+      lua_setfield(L, -2, "create_native_threads");
     }
   }
 }
 
 extern "C" int luaopen_test_address_space(lua_State* L) {
+  // std::cout << "open start " << std::this_thread::get_id() << std::endl;
   lua_newtable(L);
   brigid::initialize(L);
+  // std::cout << "open ended " << std::this_thread::get_id() << std::endl;
   return 1;
 }
