4cd6993a3ff51199140c3fbdb4b79faf659c0b6b
dev <dev@brigid.jp>
2021-10-21 19:13:25 +0900

    ファイルを分割。

---
+++ src/lua/json.cpp
@@ -0,0 +1,39 @@
+// Copyright (c) 2021 <dev@brigid.jp>
+// This software is released under the MIT License.
+// https://opensource.org/licenses/mit-license.php
+
+#include "common.hpp"
+
+#include <lua.hpp>
+
+namespace brigid {
+  namespace {
+    void impl_array(lua_State* L) {
+      if (lua_isnoneornil(L, 1)) {
+        lua_newtable(L);
+      } else {
+        lua_pushvalue(L, 1);
+      }
+      set_metatable(L, "brigid.json.array");
+    }
+  }
+
+  void initialize_json_parse(lua_State*);
+
+  void initialize_json(lua_State* L) {
+    new_metatable(L, "brigid.json.array");
+    lua_getglobal(L, "table");
+    lua_setfield(L, -2, "__index");
+    lua_pushboolean(L, true);
+    lua_setfield(L, -2, "brigid.json.array");
+    lua_pop(L, 1);
+
+    lua_newtable(L);
+    {
+      set_field(L, -1, "array", impl_array);
+
+      initialize_json_parse(L);
+    }
+    lua_setfield(L, -2, "json");
+  }
+}
