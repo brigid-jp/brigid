a26c5534f305c3aa22e43026974de8fffac8fd60
dev <dev@brigid.jp>
2021-10-26 15:31:07 +0900

    ファイルを分割。

---
+++ src/lua/ubench.hpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2021 <dev@brigid.jp>
+// This software is released under the MIT License.
+// https://opensource.org/licenses/mit-license.php
+
+#ifndef BRIGID_UBENCH_HPP
+#define BRIGID_UBENCH_HPP
+
+#include <iosfwd>
+
+namespace brigid {
+  namespace ubench {
+    void check_platform(std::ostream&);
+  }
+}
+
+#endif
