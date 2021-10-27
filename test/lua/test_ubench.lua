f760f32dca3fa1e7f868606c8d6c25607f0c8f36
dev <dev@brigid.jp>
2021-10-27 12:56:14 +0900

    テストを修正。

--- test/lua/test_ubench.lua
+++ test/lua/test_ubench.lua
@@ -12,10 +12,25 @@ function suite:test_ubench()
   print(brigid.ubench.check_runtime())
 end
 
-function suite:test_ubench_stopwatch()
+function suite:test_ubench_stopwatch1()
   local t = brigid.ubench.stopwatch()
   t:start()
   t:stop()
+  if debug then print(t:get_elapsed()) end
+end
+
+function suite:test_ubench_stopwatch2()
+  local t = brigid.ubench.stopwatch()
+  t:start()
+  t:stop() t:stop() t:stop() t:stop() t:stop() t:stop() t:stop() t:stop()
+  if debug then print(t:get_elapsed()) end
+end
+
+function suite:test_ubench_stopwatch3()
+  local t = brigid.ubench.stopwatch()
+  t:start()
+  t:stop() t:stop() t:stop() t:stop() t:stop() t:stop() t:stop() t:stop()
+  t:stop() t:stop() t:stop() t:stop() t:stop() t:stop() t:stop() t:stop()
   if debug then print(t:get_elapsed()) end
 end
 
