d099296ba6f75c8b8bbd28db14a79ee319b2104e
dev <dev@brigid.jp>
2021-10-27 09:32:08 +0900

    ubenchを修正。

--- src/lua/ubench.hpp
+++ src/lua/ubench.hpp
@@ -5,11 +5,23 @@
 #ifndef BRIGID_UBENCH_HPP
 #define BRIGID_UBENCH_HPP
 
+#include <lua.hpp>
+
+#include <stdint.h>
 #include <iosfwd>
 
 namespace brigid {
   namespace ubench {
+    class stopwatch {
+    public:
+      virtual ~stopwatch() = 0;
+      virtual void start() = 0;
+      virtual void stop() = 0;
+      virtual int64_t get_elapsed() const = 0;
+    };
+
     void check_platform(std::ostream&);
+    stopwatch* new_stopwatch_platform(lua_State*, const char*);
   }
 }
 
