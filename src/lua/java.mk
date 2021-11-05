e4057542b4a3d71d0c06b6c619f881c69190465d
dev <dev@brigid.jp>
2021-11-05 11:34:02 +0900

    ビルドスクリプトを修正。

--- src/lua/java.mk
+++ src/lua/java.mk
@@ -1,4 +1,4 @@
-# Copyright (c) 2019,2021 <dev@brigid.jp>
+# Copyright (c) 2021 <dev@brigid.jp>
 # This software is released under the MIT License.
 # https://opensource.org/licenses/mit-license.php
 
@@ -7,15 +7,29 @@ CPPFLAGS = "-I$(JAVA_HOME)/include" "-I$(JAVA_HOME)/include/$(UNAME)" -I../.. -I
 CXXFLAGS = -Wall -W -Wno-missing-field-initializers -O2 -std=c++11 -fPIC
 
 OBJS = \
-	common_java.o \
+	common.o \
 	crypto.o \
 	crypto_java.o \
+	cryptor.o \
+	data.o \
+	data_writer.o \
 	error.o \
+	file_writer.o \
+	function.o \
+	hasher.o \
 	http.o \
 	http_impl.o \
 	http_java.o \
+	json.o \
+	json_parse.o \
+	module.o \
+	scope_exit.o \
+	stack_guard.o \
 	stdio.o \
-	version.o
+	stopwatch.o \
+	stopwatch_unix.o \
+	thread_reference.o \
+	view.o
 TARGET = libbrigid.a
 
 all: all-recursive $(TARGET)
