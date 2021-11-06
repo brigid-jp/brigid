01516e894a1734ec69a9983fd642bdad2ac35168
dev <dev@brigid.jp>
2021-11-06 15:41:40 +0900

    ax_jni_include_dirはバージョン判定に問題があるので除去。

--- m4/update.sh
+++ m4/update.sh
@@ -9,7 +9,6 @@ for i in \
   ax_compare_version.m4 \
   ax_cxx_compile_stdcxx.m4 \
   ax_java_options.m4 \
-  ax_jni_include_dir.m4 \
   ax_lib_curl.m4 ax_lua.m4 \
   ax_path_generic.m4 \
   ax_prog_java.m4 \
