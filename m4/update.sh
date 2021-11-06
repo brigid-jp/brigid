0809cdf47415c372853509bfa347e9483c1c1771
dev <dev@brigid.jp>
2021-11-06 15:28:31 +0900

    ビルドスクリプトを追加。

--- m4/update.sh
+++ m4/update.sh
@@ -11,7 +11,9 @@ for i in \
   ax_lib_curl.m4 ax_lua.m4 \
   ax_path_generic.m4 \
   ax_prog_java.m4 \
-  ax_prog_javac.m4
+  ax_prog_java_works.m4 \
+  ax_prog_javac.m4 \
+  ax_prog_javac_works.m4
 do
   curl -L "https://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=blob_plain;f=m4/$i" >"$i"
 done
