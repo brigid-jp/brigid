85c071ad4ae886d81a92fa8e7cd88a7dcf2266c0
dev <dev@brigid.jp>
2021-11-06 15:24:06 +0900

    ビルドスクリプトを追加。

--- m4/update.sh
+++ m4/update.sh
@@ -1,10 +1,17 @@
 #! /bin/sh -e
 
-# Copyright (c) 2019 <dev@brigid.jp>
+# Copyright (c) 2019,2021 <dev@brigid.jp>
 # This software is released under the MIT License.
 # https://opensource.org/licenses/mit-license.php
 
-for i in ax_check_openssl.m4 ax_compare_version.m4 ax_cxx_compile_stdcxx.m4 ax_lib_curl.m4 ax_lua.m4 ax_path_generic.m4
+for i in \
+  ax_check_openssl.m4 \
+  ax_compare_version.m4 \
+  ax_cxx_compile_stdcxx.m4 \
+  ax_lib_curl.m4 ax_lua.m4 \
+  ax_path_generic.m4 \
+  ax_prog_java.m4 \
+  ax_prog_javac.m4
 do
   curl -L "https://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=blob_plain;f=m4/$i" >"$i"
 done
