cce2cb47c9c4705a4db0793a122bd120e2bf6ebd
dev <dev@brigid.jp>
2021-11-06 18:05:40 +0900

    ビルドスクリプトのJava対応を除去。

--- m4/update.sh
+++ m4/update.sh
@@ -4,18 +4,7 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/mit-license.php
 
-for i in \
-  ax_check_java_home.m4 \
-  ax_check_openssl.m4 \
-  ax_compare_version.m4 \
-  ax_cxx_compile_stdcxx.m4 \
-  ax_java_options.m4 \
-  ax_lib_curl.m4 ax_lua.m4 \
-  ax_path_generic.m4 \
-  ax_prog_java.m4 \
-  ax_prog_java_works.m4 \
-  ax_prog_javac.m4 \
-  ax_prog_javac_works.m4
+for i in ax_check_openssl.m4 ax_compare_version.m4 ax_cxx_compile_stdcxx.m4 ax_lib_curl.m4 ax_lua.m4 ax_path_generic.m4
 do
   curl -L "https://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=blob_plain;f=m4/$i" >"$i"
 done
