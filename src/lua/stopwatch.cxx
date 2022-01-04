dede11a049673df98c35ba1fdbf10aab87a24673
dev <dev@brigid.jp>
2022-01-04 18:02:19 +0900

    著作権表記を更新。

--- src/lua/stopwatch.cxx
+++ src/lua/stopwatch.cxx
@@ -2,7 +2,7 @@
 #line 1 "stopwatch.rl"
 // vim: syntax=ragel:
 
-// Copyright (c) 2021 <dev@brigid.jp>
+// Copyright (c) 2021,2022 <dev@brigid.jp>
 // This software is released under the MIT License.
 // https://opensource.org/licenses/mit-license.php
 
--- src/lua/stopwatch.rl
+++ src/lua/stopwatch.rl
@@ -1,6 +1,6 @@
 // vim: syntax=ragel:
 
-// Copyright (c) 2021 <dev@brigid.jp>
+// Copyright (c) 2021,2022 <dev@brigid.jp>
 // This software is released under the MIT License.
 // https://opensource.org/licenses/mit-license.php
 
