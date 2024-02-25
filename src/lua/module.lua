77c561f493abc172e48dda26d8c9381a62e913fd
dev <dev@brigid.jp>
2024-02-25 09:18:52 +0900

    Luaコードの追加を開始。

---
+++ src/lua/module.lua
@@ -0,0 +1,8 @@
+R""--(
+-- Copyright (c) 2024 <dev@brigid.jp>
+-- This software is released under the MIT License.
+-- https://opensource.org/licenses/mit-license.php
+
+local module = ...
+
+--)"--"
