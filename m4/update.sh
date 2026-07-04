8aba5058d63f2b474226408cc7a6c47fde070b3f
dev <dev@brigid.jp>
2026-07-04 21:00:37 +0900

    GitHub Mirrorからダウンロードするように変更。

--- m4/update.sh
+++ m4/update.sh
@@ -1,12 +1,13 @@
 #! /bin/sh -e
 
-# Copyright (c) 2019,2021,2024 <dev@brigid.jp>
+# Copyright (c) 2019,2021,2024,2026 <dev@brigid.jp>
 # This software is released under the MIT License.
 # https://opensource.org/licenses/mit-license.php
 
 for i in ax_check_openssl.m4 ax_compare_version.m4 ax_cxx_compile_stdcxx.m4 ax_lib_curl.m4 ax_lua.m4 ax_path_generic.m4
 do
-  curl -L "https://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=blob_plain;f=m4/$i" >"$i"
+  # curl -L "https://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=blob_plain;f=m4/$i" >"$i"
+  curl -LO "https://raw.githubusercontent.com/autoconf-archive/autoconf-archive/refs/heads/master/m4/$i"
 done
 
-curl -LO https://git.savannah.gnu.org/cgit/gnulib.git/plain/m4/ld-version-script.m4
+curl -LO https://raw.githubusercontent.com/coreutils/gnulib/refs/heads/master/m4/ld-version-script.m4
